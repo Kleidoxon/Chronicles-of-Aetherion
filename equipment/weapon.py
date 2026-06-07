af289f5853d897bcab4b9edfb60852d3b4ca3303
Kleidoxon <109430706+Kleidoxon@users.noreply.github.com>
2026-06-07 18:19:39 +0700

    Update weapon.py

--- equipment/weapon.py
+++ equipment/weapon.py
@@ -1,21 +1,41 @@
 # equipment/weapon.py
 
-from equipment.equipment_slot import EquipmentSlot
+from equipment.equipment_base import (
+    EquipmentBase
+)
+
+from equipment.equipment_slot import (
+    EquipmentSlot
+)
 
 
-class Weapon:
+class Weapon(EquipmentBase):
 
     def __init__(
         self,
         name,
         rarity,
-        attack_bonus
+        attack_bonus,
+        item_level=1,
+        required_level=1,
+        value=0
     ):
 
-        self.name = name
+        super().__init__(
 
-        self.rarity = rarity
+            name=name,
 
-        self.attack_bonus = attack_bonus
+            rarity=rarity,
 
-        self.slot = EquipmentSlot.WEAPON
+            slot=EquipmentSlot.WEAPON,
+
+            item_level=item_level,
+
+            required_level=required_level,
+
+            value=value
+        )
+
+        self.attack_bonus = (
+            attack_bonus
+        )
